b67ad8ce5d87f4e18a303d054df8887afb4892d3
Swastika Mishra <swastika.mishra2022@vitstudent.ac.in>
2024-05-29 21:03:41 +0530

    Recommit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
-import Navbar from "react-bootstrap/Navbar";
-import Nav from "react-bootstrap/Nav";
-import Container from "react-bootstrap/Container";
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container';
 import { Link } from "react-router-dom";
 
 import {
