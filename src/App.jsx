0a779f80db13d3221622b7062b8a3a5c1ebee028
Swastika Mishra <swastika.mishra2022@vitstudent.ac.in>
2024-05-29 20:35:30 +0530

    Recommit

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ import {
 import ScrollToTop from "./components/ScrollToTop";
 import "./style.css";
 import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   const [load, upadateLoad] = useState(true);
